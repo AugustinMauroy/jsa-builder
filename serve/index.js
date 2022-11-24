6ee6c47b44ad4725c4fc0f64623088776d87d315
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-11-24 19:42:23 +0100

    feat(render) firs version
    * not working

--- serve/index.js
+++ serve/index.js
@@ -1,3 +1,41 @@
-export function serve(){
+import http from 'node:http';
+import fs from 'node:fs';
+import { build } from '../build/index.js';
 
+export function serve(){
+    build()
+
+    const server = http.createServer((req,res)=>{
+        res.statusCode = 200
+        res.setHeader('Content-Type', contentType(req.url))
+        res.end(fileRouting(req.url))    
+    });
+
+    server.listen(8080,() => {
+        console.log(`Server running at http://localhost:8080/`)
+    });
+};
+
+function fileRouting(url){
+    if (url === '/') {
+        fs.readFileSync('./_site/index.html', 'utf-8',(err,data)=>{
+            if (err) {
+                console.error(err);;
+            } else {
+                return data;
+            }
+        })
+    } else {
+        fs.readFileSync(`./_site/${url}`, 'utf-8',(err,data)=>{
+            if (err) {
+                console.error(err);;
+            } else {
+                return data;
+            }
+        })
+    }
+};
+
+function contentType(url){
+    return 'text/html'
 };

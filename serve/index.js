c439b60cd5d8312b05987b07f7ff901aeba7f875
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-12-01 22:19:50 +0100

    chore(npm) add git

--- serve/index.js
+++ serve/index.js
@@ -2,45 +2,16 @@ import http from 'node:http';
 import fs from 'node:fs';
 import { build } from '../build/index.js';
 
-export async function serve(){
-    try{
-        build()
-    } finally {
-    const server = http.createServer((req,res)=>{
+export function serve(){
+    build()
+
+    const server = http.createServer(async (req,res) => {
         res.statusCode = 200
-        res.setHeader('Content-Type', contentType(req.url))
-        let data = fileRouting(req.url);
-        res.end(data);    
+        res.setHeader('Content-Type', 'text/html')
+        res.end("<h1>Hello word!</h1>");    
     });
 
     server.listen(8080,() => {
-        console.log(`Server running at http://localhost:8080/`)
+        console.log("\x1b[32m",'Server running at',"\x1b[1m",'http://localhost:8080/',"\x1b[0m")
     });
-    }
 };
-
-function fileRouting(url){
-    if (url === '/') {
-        return fs.readFileSync('./_site/index.html', 'utf-8',(err,data)=>{
-            if (err) {
-                return 'error404'
-            } else {
-                return data;
-            }
-        })
-    } else {
-        return fs.readFileSync(`./_site${url}.html`, 'utf-8',(err,data)=>{
-            if (err) {
-                return 'error404'
-            } else {
-                return data;
-            }
-        })
-    }
-};
-
-function contentType(url){
-    return 'text/html'
-};
-
-

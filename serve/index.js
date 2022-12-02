2b52d4c5a1b52a688903fda4c1760cd95d4fa371
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2022-12-02 22:18:01 +0100

    First part of serve

--- serve/index.js
+++ serve/index.js
@@ -2,16 +2,78 @@ import http from 'node:http';
 import fs from 'node:fs';
 import { build } from '../build/index.js';
 
-export function serve(){
-    build()
 
-    const server = http.createServer(async (req,res) => {
+export async function serve(){
+    build();
+
+    const server = http.createServer(async (req, res)=>{
+        let url = req.url;
         res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        res.end("<h1>Hello word!</h1>");    
-    });
+        res.setHeader('Content-Type', 'text/plain')
+        res.end("Hello World!")
+        console.log(getInfo(url));
+    })
 
-    server.listen(8080,() => {
-        console.log("\x1b[32m",'Server running at',"\x1b[1m",'http://localhost:8080/',"\x1b[0m")
+    server.listen(8080, ()=>{
+        console.log(`Server running at http://localhost:8080`)
     });
 };
+
+async function getInfo(path){
+    if (path === '/') {
+        return {
+            path: './_site/index.html',
+            contentType: 'text/html',
+        }
+    } else {
+        if (isDirectory(path)) {
+            if (indexHtml(path)){
+                return {
+                    path: `./_site${path}/index.html`,
+                    contentType: 'text/html',
+                }
+            }
+        }
+        if (isFile(path)) {
+            return{
+                path: `./_site${path}`,
+                contentType: checkContent(path),
+            }
+        }
+    }
+}
+
+async function isDirectory(path){
+    try {
+        fs.readdir(`./_site${path}`)
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+async function indexHtml(path){
+    try {
+        fs.readFile(`./site${path}/index.html`,'utf-8')
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+async function isFile(path){
+    try {
+        fs.readFile(`./site${path}`,'utf-8')
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+async function checkContent(path){
+    try{
+        
+    } catch (e) {
+        return 'text/plain';
+    }
+}
